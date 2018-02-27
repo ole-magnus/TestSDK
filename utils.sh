b099e0d9c7b122e36dd053e310dd50230d7cb8e2
olemaga <ole.magnus.aker@gmail.com>
2018-02-27 16:01:05 +0100

    Fix

--- utils.sh
+++ utils.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 last_author="$(git log -1 --pretty=format:'%an')"
+echo "last author is: $last_author"
 if [ "$last_author" == "olemaga" ]; then
   export IS_OLE_MAGNUS=YES 
 else
