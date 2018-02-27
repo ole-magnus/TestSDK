5ecd0002b6a6c27efac122228be75fba6afa836c
olemaga <ole.magnus.aker@gmail.com>
2018-02-27 15:56:34 +0100

    Test

--- utils.sh
+++ utils.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-is_ole_magnus() {
-  local last_author="$(git log -1 --pretty=format:'%an')"
-  if [ "$last_author" == "olemaga" ]; then
-    echo "YES" 
-  else
-    echo "NO"
-  fi
-}
-export IS_OLE_MAGNUS=$(is_ole_magnus)
+last_author="$(git log -1 --pretty=format:'%an')"
+if [ "$last_author" == "olemaga" ]; then
+  export IS_OLE_MAGNUS=YES 
+else
+  export IS_OLE_MAGNUS=NO
+fi
+
